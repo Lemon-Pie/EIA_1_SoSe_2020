624d3510ee6f4047040aa2d19291631f9cb4004f
Lemon-Pie <64230161+Lemon-Pie@users.noreply.github.com>
2020-05-29 20:02:59 +0200

    #5 und Steckbrief Link Update

---
+++ Aufgaben/TypeScript/script.ts
@@ -0,0 +1,164 @@
+console.group("Europa");
+
+var europe08: number = 4965.7;
+var europe18: number = 4209.3;
+
+var relativeu: number = 100 / (49010.4 / 4209.3);
+var growtheu: number = (europe18 - europe08) / europe08;
+var growthEUabsolute: number = europe18 - europe08;
+
+var eu1: string = "Die Emission von Europa war 2018:";
+var eu2: string = "kg CO2.";
+var eu3: string = "Relativ zur Gesamtemission der Welt verursachte Europa damit";
+var eu4: string = "%.";
+var eu5: string = "Für Europa hatte sich 2018 im Vergleich zu 2008 die Emission um";
+var eu6: string = "% verändert.";
+var eu7: string = "2018 im Vergleich zu 2008 waren das";
+var eu8: string = "kg CO2.";
+
+console.log(eu1 + " " + europe18 + eu2);
+console.log(eu3 + " " + relativeu + eu4);
+console.log(eu5 + " " + growtheu + eu6);
+console.log(eu7 + " " + growthEUabsolute + eu8);
+
+console.groupEnd("Europa");
+console.log("");
+
+
+
+console.group("Nord Amerika");
+
+var na08: number = 6600.4;
+var na18: number = 6035.6;
+
+var relativna: number = 100 / (67756.80 / 6035.6);
+var growthna: number = (na18 - na08) / na08;
+var growthNAabsolute: number = na18 - na08;
+
+var na1: string = "Die Emission von Nord Amerika war 2018:";
+var na2: string = "kg CO2.";
+var na3: string = "Relativ zur Gesamtemission der Welt verursachte Europa damit";
+var na4: string = "%.";
+var na5: string = "Für Europa hatte sich 2018 im Vergleich zu 2008 die Emission um";
+var na6: string = "% verändert.";
+var na7: string = "2018 im Vergleich zu 2008 waren das";
+var na8: string = "kg CO2.";
+
+console.log(na1 + " " + na18 + na2);
+console.log(na3 + " " + relativna + na4);
+console.log(na5 + " " + growthna + na6);
+console.log(na7 + " " + growthNAabsolute + na8);
+
+console.groupEnd("Nord Amerika");
+console.log("");
+
+
+
+console.group("Süd Amerika");
+
+var sa08: number = 1132.6;
+var sa18: number = 1261.5;
+
+var relativsa: number = 100 / (13664.70 / 1261.5);
+var growthsa: number = (sa18 - sa08) / sa08;
+var growthSAabsolute: number = sa18 - sa08;
+
+var sa1: string = "Die Emission von Nord Amerika war 2018:";
+var sa2: string = "kg CO2.";
+var sa3: string = "Relativ zur Gesamtemission der Welt verursachte Europa damit";
+var sa4: string = "%.";
+var sa5: string = "Für Europa hatte sich 2018 im Vergleich zu 2008 die Emission um";
+var sa6: string = "% verändert.";
+var sa7: string = "2018 im Vergleich zu 2008 waren das";
+var sa8: string = "kg CO2.";
+
+console.log(sa1 + " " + sa18 + sa2);
+console.log(sa3 + " " + relativsa + sa4);
+console.log(sa5 + " " + growthsa + sa6);
+console.log(sa7 + " " + growthSAabsolute + sa8);
+
+console.groupEnd("Süd Amerika");
+console.log("");
+
+
+
+console.group("Afrika");
+
+var af08: number = 1028;
+var af18: number = 1235.50;
+
+var relativaf: number = 100 / (12433.40 / 1235.50);
+var growthaf: number = (af18 - af08) / af08;
+var growthAFabsolute: number = af18 - af08;
+
+var af1: string = "Die Emission von Nord Amerika war 2018:";
+var af2: string = "kg CO2.";
+var af3: string = "Relativ zur Gesamtemission der Welt verursachte Europa damit";
+var af4: string = "%.";
+var af5: string = "Für Europa hatte sich 2018 im Vergleich zu 2008 die Emission um";
+var af6: string = "% verändert.";
+var af7: string = "2018 im Vergleich zu 2008 waren das";
+var af8: string = "kg CO2.";
+
+console.log(af1 + " " + af18 + af2);
+console.log(af3 + " " + relativaf + af4);
+console.log(af5 + " " + growthaf + af6);
+console.log(af7 + " " + growthAFabsolute + af8);
+
+console.groupEnd("Afrika");
+console.log("");
+
+
+
+console.group("Asien");
+
+var asi08: number = 12954.70;
+var asi18: number = 16274.10;
+
+var relativasi: number = 100 / (166214.30 / 16274.10);
+var growthasi: number = (asi18 - asi08) / asi08;
+var growthASIabsolute: number = asi18 - asi08;
+
+var asi1: string = "Die Emission von Nord Amerika war 2018:";
+var asi2: string = "kg CO2.";
+var asi3: string = "Relativ zur Gesamtemission der Welt verursachte Europa damit";
+var asi4: string = "%.";
+var asi5: string = "Für Europa hatte sich 2018 im Vergleich zu 2008 die Emission um";
+var asi6: string = "% verändert.";
+var asi7: string = "2018 im Vergleich zu 2008 waren das";
+var asi8: string = "kg CO2.";
+
+console.log(asi1 + " " + asi18 + asi2);
+console.log(asi3 + " " + relativasi + asi4);
+console.log(asi5 + " " + growthasi + asi6);
+console.log(asi7 + " " + growthASIabsolute + asi8);
+
+console.groupEnd("Asien");
+console.log("");
+
+
+
+console.group("Australien");
+
+var aus08: number = 1993;
+var aus18: number = 2100.50;
+
+var relativaus: number = 100 / (22034.50 / 2100.50);
+var growthaus: number = (aus18 - aus08) / aus08;
+var growthAUSabsolute: number = aus18 - aus08;
+
+var aus1: string = "Die Emission von Nord Amerika war 2018:";
+var aus2: string = "kg CO2.";
+var aus3: string = "Relativ zur Gesamtemission der Welt verursachte Europa damit";
+var aus4: string = "%.";
+var aus5: string = "Für Europa hatte sich 2018 im Vergleich zu 2008 die Emission um";
+var aus6: string = "% verändert.";
+var aus7: string = "2018 im Vergleich zu 2008 waren das";
+var aus8: string = "kg CO2.";
+
+console.log(aus1 + " " + aus18 + aus2);
+console.log(aus3 + " " + relativaus + aus4);
+console.log(aus5 + " " + growthaus + aus6);
+console.log(aus7 + " " + growthAUSabsolute + aus8);
+
+console.groupEnd("Australien");
